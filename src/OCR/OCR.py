24c1f50994235de90dd44390dc53e74f7daf5209
TylerLott <tylerlott0126@gmail.com>
2021-06-30 08:24:14 -0600

    cleaned up ocr a bit more

--- src/OCR/OCR.py
+++ src/OCR/OCR.py
@@ -22,19 +22,19 @@ class OCR:
     """
 
     def __init__(self, image_path):
-        self.image = cv2.imread(image_path, 0)
-        self.bitnot, self.bounding_boxes = get_boxes(self.image)
+        self._image = cv2.imread(image_path, 0)
 
-    def get_bitnot_img(self):
-        """ "return the image modified to not have table lines"""
-        return self.bitnot
+    def get_image(self):
+        """GETTER"""
+        return self._image
 
     def extract_table(self):
         """
         Run table extraction and return array of shape(rows, columns)
         """
+        bitnot, bounding_boxes = get_boxes(self._image)
         row = []
-        for i in self.bounding_boxes:
+        for i in bounding_boxes:
             for j in i:
                 if len(j) == 0:
                     row.append(["", -2])
@@ -42,36 +42,32 @@ class OCR:
                     col = []
                     for k in j:
                         y, x, w, h = (k[0], k[1], k[2], k[3])
-                        cropped_img = self.bitnot[x : x + h, y : y + w]
+                        cropped_img = bitnot[x : x + h, y : y + w]
                         col = _run_tesseract(cropped_img)
                     row.append(col)
 
         arr = np.array(row)
-        return arr.reshape(
-            (len(self.bounding_boxes), len(self.bounding_boxes[0]), 2)
-        ).tolist()
+        return arr.reshape((len(bounding_boxes), len(bounding_boxes[0]), 2)).tolist()
 
 
 def _run_tesseract(image):
+    tesseract_config = """-c tessedit_char_whitelist=
+        "01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz.-/ '"
+        --psm 6 --oem 1"""
     out = pytesseract.image_to_data(
         image,
         lang="eng",
-        config='-c tessedit_char_whitelist=" '
-        '01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz.-/" '
-        " --psm 6 --oem 1",
+        config=tesseract_config,
         output_type=pytesseract.Output.DICT,
     )
     ind = np.where(np.array(out.get("conf")) != "-1")
     text = ""
     conf = 0
-    if len(ind[0]) > 1:
+    if len(ind[0]) >= 1:
         for i in ind[0]:
-            text = text + out.get("text")[i] + " "
+            text = " ".join([text, out.get("text")[i]])
             conf += float(out.get("conf")[i])
         conf = conf / len(ind[0])
-    elif len(ind[0]) == 1:
-        text = out.get("text")[ind[0][0]]
-        conf = float(out.get("conf")[ind[0][0]])
     if text == "" and conf == 0:
         conf = -2  # this denotes a empty space predition
     return [text, conf]
